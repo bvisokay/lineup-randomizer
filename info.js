ded9ecd9cc61435961ee830e7c53027b863df6cb
Brandon Visokay <hello@brandonvisokay.com>
2022-10-02 10:12:47 -0400

    Week-04 data

--- info.js
+++ info.js
@@ -1,8 +1,8 @@
 // There is default text in the HTML but the JS overrides with the information below
 let info = {
-  lastUpdated: "Projection data last updated: 09/18/22 @ 10:00am",
-  captainSlate: "09/18/22 8:20pm (CHI vs GB) 1 Game",
-  mainSlate: "09/18/22 1:00pm (All Games) 12 games",
-  earlySlate: "09/18/22 1:00pm (Ealy Only) 7 Games",
-  lateSlate: "09/18/22 4:05pm (Late Only) 5 Games"
+  lastUpdated: "Projection data last updated: 10/2/22 @ 9:45am",
+  captainSlate: "10/2/22 8:20pm (KC @ TB) 1 Game",
+  mainSlate: "10/2/22 1:00pm (All Games) 12 games",
+  earlySlate: "10/2/22 1:00pm (Ealy Only) 9 Games",
+  lateSlate: "10/2/22 4:05pm (Late Only) 3 Games"
 }
