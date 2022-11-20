0204785d5981dae4290c4fadd6bfeb2bcc5fb857
Brandon Visokay <hello@brandonvisokay.com>
2022-11-20 11:56:21 -0500

    Week-11 data

--- info.js
+++ info.js
@@ -1,8 +1,8 @@
 // There is default text in the HTML but the JS overrides with the information below
 let info = {
-  lastUpdated: "Projection data last updated: 10/2/22 @ 9:45am",
-  captainSlate: "10/2/22 8:20pm (KC @ TB) 1 Game",
-  mainSlate: "10/2/22 1:00pm (All Games) 12 games",
-  earlySlate: "10/2/22 1:00pm (Ealy Only) 9 Games",
-  lateSlate: "10/2/22 4:05pm (Late Only) 3 Games"
+  lastUpdated: "Projection data last updated: 11/20/22 @ 9:06am",
+  captainSlate: "11/20/22 8:20pm (KC @ LAC) 1 Game",
+  mainSlate: "11/20/22 1:00pm (All Games) 11 games",
+  earlySlate: "11/20/22 1:00pm (Ealy Only) 8 Games",
+  lateSlate: "11/20/22 4:05pm (Late Only) 3 Games"
 }
