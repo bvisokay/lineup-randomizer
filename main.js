66ae97d04169ae61f33d0897648814099ca6c34c
Brandon Visokay <hello@brandonvisokay.com>
2023-04-25 21:20:46 -0400

    Move addStartSelectOptions FN to external file

--- main.js
+++ main.js
@@ -45,26 +45,7 @@ const trimmedPlayerData = playerData.map(player => {
   }
 })
 
-// Add start select options
-function addStartSelectOptions(selectVariable, positionString) {
-  trimmedPlayerData.sort((a, b) => {
-    if (a.name > b.name) {
-      return 1
-    }
-    if (a.name < b.name) {
-      return -1
-    }
-  })
-  selectVariable.innerHTML = `
-  <option value="">Select</option>
-  ${trimmedPlayerData
-    .filter(x => x.pos == positionString)
-    .map(player => {
-      return `<option value="${player.name}">${player.name}</option>`
-    })}
-    `
-}
-
+// set data in select options
 addStartSelectOptions(qbSelect, "QB")
 addStartSelectOptions(rbSelect, "RB")
 addStartSelectOptions(wrSelect, "WR")
